191cb911215643e58190310f4c3c43c1eef65b91
serhiirodikov <sergiy1998373@gmail.com>
2020-12-22 21:54:28 +0200

    added some components and fix bugs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,81 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
+import Library from './components/Library'
+import Nav from './components/Nav'
 import Player from './components/Player'
 import Song from './components/Song'
 import './styles/app.scss'
+import data from './util'
+
 function App() {
+  const audioRef = useRef(null)
+
+  const [songs, setSongs] = useState(data())
+  const [libraryStatus, setLibraryStatus] = useState(false)
+  const [currentSong, setCurrentSong] = useState(songs[0])
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  })
+
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime
+    const duration = e.target.duration
+    setSongInfo({ ...songInfo, currentTime: current, duration })
+  }
+
+  const setEndhandler = async () => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id)
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length])
+    if (isPlaying) audioRef.current.play()
+  }
+  useEffect(() => {
+    function onPlay(e) {
+      if (e.key === ' ') {
+        if (isPlaying) {
+          audioRef.current.pause()
+          setIsPlaying(false)
+        } else {
+          audioRef.current.play()
+          setIsPlaying(true)
+        }
+      }
+    }
+    window.addEventListener('keypress', onPlay)
+    return () => {
+      window.removeEventListener('keypress', onPlay)
+    }
+  }, [isPlaying])
+
   return (
     <div className="App">
-      <h1>Music Player</h1>
-      <Song />
-      <Player />
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+        setCurrentSong={setCurrentSong}
+        audioRef={audioRef}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo}
+        songs={songs}
+        setSongs={setSongs}
+      />
+      <Library
+        libraryStatus={libraryStatus}
+        audioRef={audioRef}
+        songs={songs}
+        setCurrentSong={setCurrentSong}
+        setIsPlaying={setIsPlaying}
+        setSongs={setSongs}
+      />
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+        onEnded={setEndhandler}
+      ></audio>
     </div>
   )
 }
---
+++ src/util.js
@@ -0,0 +1,66 @@
+import { v4 as uuidv4 } from 'uuid'
+function chillHop() {
+  return [
+    {
+      name: 'Hereafter',
+      artist: 'Marko',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=11770',
+      id: uuidv4(),
+      active: true,
+      color: ['#39538F', '#C77F7F'],
+    },
+    {
+      name: 'Seascape',
+      artist: 'Marko',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=11773',
+      id: uuidv4(),
+      active: false,
+      color: ['#39538F', '#C77F7F'],
+    },
+    {
+      name: 'Melancholy',
+      artist: 'Marko',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=11778',
+      id: uuidv4(),
+      active: false,
+      color: ['#39538F', '#C77F7F'],
+    },
+    {
+      name: 'Oasis',
+      artist: 'Marko',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=11768',
+      id: uuidv4(),
+      active: false,
+      color: ['#39538F', '#C77F7F'],
+    },
+    {
+      name: 'Longing',
+      artist: 'Marko',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=11777',
+      id: uuidv4(),
+      active: false,
+      color: ['#39538F', '#C77F7F'],
+    },
+    {
+      name: 'Departure',
+      artist: 'Marko',
+      cover:
+        'https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg',
+      audio: 'https://mp3.chillhop.com/serve.php/?mp3=11771',
+      id: uuidv4(),
+      color: ['#39538F', '#C77F7F'],
+      active: false,
+    },
+  ]
+}
+export default chillHop
