e70b456ab555d1b977630ad936d0a3aa27aa3d2d
serhiirodikov <sergiy1998373@gmail.com>
2020-12-22 21:54:03 +0200

    [CREATED] Player component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,23 +1,103 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faPlay,
   faAngleLeft,
   faAngleRight,
+  faPause,
 } from '@fortawesome/free-solid-svg-icons'
 
-const Player = () => {
+const Player = ({
+  currentSong,
+  setIsPlaying,
+  isPlaying,
+  audioRef,
+  setSongInfo,
+  songInfo,
+  songs,
+  setCurrentSong,
+  setSongs,
+}) => {
+  const playSongHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause()
+      setIsPlaying(!isPlaying)
+    } else {
+      audioRef.current.play()
+      setIsPlaying(!isPlaying)
+    }
+  }
+  useEffect(() => {
+    const newSong = songs.map((song) =>
+      song.id === currentSong.id
+        ? { ...song, active: true }
+        : { ...song, active: false }
+    )
+    console.log('hi')
+    setSongs(newSong)
+    // eslint-disable-next-line
+  }, [currentSong.id])
+
+  const getTime = (time) => {
+    return Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
+  }
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value
+    setSongInfo({ ...songInfo, currentTime: e.target.value })
+  }
+  const skipTrackHandler = async (direction) => {
+    let currentIndex = songs.findIndex((song) => song.id === currentSong.id)
+    if (direction === 'skip-forward') {
+      await setCurrentSong(songs[(currentIndex + 1) % songs.length])
+      playAudio(isPlaying, audioRef)
+    }
+    if (direction === 'skip-back') {
+      if ((currentIndex - 1) % songs.length === -1) {
+        await setCurrentSong(songs[songs.length - 1])
+        playAudio(isPlaying, audioRef)
+        return
+      }
+      await setCurrentSong(songs[(currentIndex - 1) % songs.length])
+      playAudio(isPlaying, audioRef)
+    }
+  }
+
+  const playAudio = async (isPlaying, audioRef) => {
+    if (isPlaying) {
+      await audioRef.current.play()
+    } else {
+      await audioRef.current.pause()
+    }
+  }
+
   return (
     <div className="player">
       <div className="time-control">
-        <p>Start Time</p>
-        <input type="range" />
-        <p>End Time</p>
+        <p>{getTime(songInfo.currentTime)}</p>
+        <input
+          onChange={dragHandler}
+          min={0}
+          max={songInfo.duration || 0}
+          value={songInfo.currentTime}
+          type="range"
+        />
+        <p>{songInfo.duration ? getTime(songInfo.duration) : '0:00'}</p>
       </div>
       <div className="play-control">
-        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
         <FontAwesomeIcon
+          onClick={() => skipTrackHandler('skip-back')}
+          className="skip-back"
+          size="2x"
+          icon={faAngleLeft}
+        />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={!isPlaying ? faPlay : faPause}
+        />
+        <FontAwesomeIcon
+          onClick={() => skipTrackHandler('skip-forward')}
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
